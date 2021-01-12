e83483c30c95822cc0326ce2f083e93340dcca21
Ayushmann <63894266+ayushman768@users.noreply.github.com>
2021-01-12 23:27:07 +0530

    Add files via upload

---
+++ javascript/custum.js
@@ -0,0 +1,14 @@
+var navigation = document.querySelector('nav');
+
+function my_tab() {
+
+    if (navigation.style.display === "none") {
+        navigation.style.display = "block";
+
+    } else {
+        navigation.style.display = "none";
+
+    }
+
+
+}
